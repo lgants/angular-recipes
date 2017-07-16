f1e1155a2f36cd0f69faeb596735549f9148c042
lgants <lgants@gmail.com>
2017-07-16 18:15:24 -0500

    completed basic app component layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,22 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingAddedComponent } from './shopping-list/shopping-added/shopping-added.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingAddedComponent
   ],
   imports: [
     BrowserModule,
