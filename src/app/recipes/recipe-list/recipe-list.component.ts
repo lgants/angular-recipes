88f872f3f0316cee88d2e373cef12b62b1411996
lgants <lgants@gmail.com>
2017-07-16 22:00:11 -0500

    updated header

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  // square brackets after recipes tells TS that recipes will be an array of instances of class Recipe
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test', 'https://static.pexels.com/photos/70497/pexels-photo-70497.jpeg')
+  ];
 
   constructor() { }
 
