1903cee1486172f0b518bffddc9c06cf4c09f06b
lgants <lgants@gmail.com>
2017-07-29 18:27:40 -0500

    improved navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
+  // creating a custom featureSelected event listener
+  // @Output() enables the parent component to listen for an event
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 
 }
