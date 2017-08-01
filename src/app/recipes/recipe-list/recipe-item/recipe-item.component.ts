080f32bd327a894132bbfbf27ea4b965955b1c83
lgants <lgants@gmail.com>
2017-07-31 20:05:09 -0500

    performed debugging

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -6,8 +6,7 @@ import { RecipeService } from '../../recipe.service';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css'],
-  providers: [RecipeService]
+  styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
   // NOTE @Input enables binding this component property from outside
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,40 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a test', 'https://static.pexels.com/photos/70497/pexels-photo-70497.jpeg'),
-    new Recipe('Another Test Recipe', 'This is another test', 'https://static.pexels.com/photos/70497/pexels-photo-70497.jpeg')
+    new Recipe(
+      'Test Recipe',
+      'This is a test', 'https://static.pexels.com/photos/70497/pexels-photo-70497.jpeg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Bun', 1),
+        new Ingredient('Lettuce', 1),
+      ]
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is another test', 'https://static.pexels.com/photos/70497/pexels-photo-70497.jpeg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Bun', 1),
+        new Ingredient('Lettuce', 1),
+      ]
+    )
   ];
+
+  // constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     // slice without an argument will return a new recipe array rather than the same array
     return this.recipes.slice();
   }
+
+  // addIngredientsToShoppingList(ingredients: Ingredient[]) {
+  //   this.slService.addIngredients(ingredients);
+  // }
 }
