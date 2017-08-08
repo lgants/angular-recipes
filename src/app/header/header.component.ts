484d37499bd030c714c119398931a5d324c3de50
lgants <lgants@gmail.com>
2017-08-07 22:01:20 -0500

    added a http

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, Output } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -10,4 +13,18 @@ export class HeaderComponent {
   // creating a custom featureSelected event listener
   // @Output() enables the parent component to listen for an event
   // @Output() featureSelected = new EventEmitter<string>();
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      )
+  }
+
+  onFetchData() {
+    this.dataStorageService.getRecipes();
+  }
 }
